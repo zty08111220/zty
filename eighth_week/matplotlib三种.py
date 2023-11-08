e4408ef9d11a1fec56f06591e20d03719d7b3e29
zty08111220 <10225501453@stu.ecnu.edu.cn>
2023-11-08 19:29:58 +0800

    homework

---
+++ eighth_week/matplotlib三种.py
@@ -0,0 +1,37 @@
+import matplotlib.pyplot as plt
+
+# 折线图
+def plot_line():
+    x = [1, 2, 3, 4, 5]
+    y = [2, 4, 6, 8, 10]
+    plt.plot(x, y)
+    plt.title("折线图")
+    plt.xlabel("X轴")
+    plt.ylabel("Y轴")
+    plt.show()
+
+# 散点图
+def plot_scatter():
+    x = [1, 2, 3, 4, 5]
+    y = [2, 4, 6, 8, 10]
+    plt.scatter(x, y)
+    plt.title("散点图")
+    plt.xlabel("X轴")
+    plt.ylabel("Y轴")
+    plt.show()
+
+# 柱状图
+def plot_bar():
+    x = [1, 2, 3, 4, 5]
+    y = [2, 4, 6, 8, 10]
+    plt.bar(x, y)
+    plt.title("柱状图")
+    plt.xlabel("X轴")
+    plt.ylabel("Y轴")
+    plt.show()
+
+# 在主函数中调用绘图函数
+if __name__ == "__main__":
+    plot_line()
+    plot_scatter()
+    plot_bar()
